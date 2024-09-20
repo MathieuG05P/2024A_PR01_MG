6ef89ac34068798dd55108cc3800840d00f638e3
MathieuGuimond <mathieu-2.guimond@polymtl.ca>
2024-09-20 08:35:21 -0400

    Terminé

--- ghost.py
+++ ghost.py
@@ -30,28 +30,16 @@ class Ghost:
     def move(self):
         # Si le fantôme n'est pas "mort", commencez le calcul de sa prochaine position
         if not self.dead:
+            
+            
             next_x = self.pos[0] + self.direction[0] * self.speed
             next_y = self.pos[1] + self.direction[1] * self.speed
-            # TODO: Calculer la prochaine position en fonction de la direction et de la vitesse
-            # Utilisez `self.direction` pour déterminer la direction et `self.speed` pour le déplacement.
-            # La formule pour calculer la prochaine position est la suivante:
-            # next_x = self.pos[0] + self.direction[0] * self.speed
-            # Créer un rectangle pour la prochaine position prévue
-            # Utilisez pygame.Rect pour créer un rectangle représentant la position prévue du fantôme.
-            next_rect = pygame.Rect(next_x, next_y, GHOST_SIZE[0], GHOST_SIZE[1]) #colision voir...
-            #next_rect = pygame.Rect(next_x, next_y, GHOST_SIZE[0], GHOST_SIZE[1])
+            next_rect = pygame.Rect(next_x, next_y, GHOST_SIZE[0], GHOST_SIZE[1])
             if self.check_collision(next_rect):
-            
-            # TODO Vérifier si la prochaine position entre en collision avec un mur
-            # Utilisez `self.check_collision()` pour détecter si le fantôme va heurter un mur. #inverser
                 self.change_direction()
-                # TODO: Si aucune collision n'est détectée, mettre à jour la position du fantôme
-            
-                # TODO: Changer la direction du fantôme s'il rencontre un mur
-            else:
-                self.pos[0],  self.pos[1] = next_x, next_y
-                self.rect.topleft = (self.pos[0], self.pos[1])
-        # Gérer le cas où le fantôme est "mort" avec un timer pour sa résurrection
+            if not self.check_collision(next_rect):
+                self.pos = (next_x, next_y)
+                self.rect = next_rect
         elif self.death_timer > 0:
             self.death_timer -= 1
 
@@ -78,21 +66,15 @@ class Ghost:
         self.death_timer = 65
 
     def change_direction(self):
-        directions =  [Direction.RIGHT, Direction.LEFT, Direction.UP, Direction.DOWN]
-        # TODO: Créer une liste de toutes les directions possibles pour le fantôme (gauche, droite, haut, bas)
+        directions =  [Direction.LEFT, Direction.RIGHT, Direction.UP, Direction.DOWN]
         random.shuffle(directions)
-        # TODO: Mélanger aléatoirement les directions pour simuler un choix aléatoire avec `random.shuffle()`
-        for direction in directions:
-        # TODO: Parcourir chaque direction et vérifier si elle est valide (pas de collision avec un mur)
-            # TODO: Calculer la prochaine position du fantôme en fonction de la direction
-            next_x = self.pos[0] + direction[0] * self.speed
-            next_y = self.pos[1] + direction[1] * self.speed
-            #ßCréer un rectangle représentant cette nouvelle position
-            #next_rect = pygame.Rect(next_x, next_y, GHOST_SIZE[0], GHOST_SIZE[1])
+        self.pos = (int(self.pos[0] / TILE_WIDTH) * TILE_WIDTH, int(self.pos[1] / TILE_HEIGHT) * TILE_HEIGHT)
+        for choice in directions:
+            next_y = self.pos[1] + choice[1] * self.speed
+            next_x = self.pos[0] + choice[0] * self.speed
             next_rect = pygame.Rect(next_x, next_y, GHOST_SIZE[0], GHOST_SIZE[1])
-            # TODO: Vérifier si cette direction entraîne une collision avec un mur en utilisant `self.check_collision()`
             if not self.check_collision(next_rect):
-                self.direction = direction # TODO: Si aucune collision n'est détectée, définir cette direction comme la nouvelle direction du fantôme avec `self.set_direction()` et sortir de la boucle
+                self.direction = choice
                 return # Sortir de la méthode une fois la direction changée
 
     def stop(self):
